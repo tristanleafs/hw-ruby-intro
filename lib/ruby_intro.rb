52d589f304490dd842b37c62a1f32c83e7db156e
Tristan <tristan.lee@theleehome.org>
2021-10-08 01:15:56 +0000

    done summ work

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -6,16 +6,37 @@ def sum arr
   # YOUR CODE HERE
   sum = 0
   for i in arr do
-    sum += arr
+    sum += i
+  end
   return sum
 end
 
 def max_2_sum arr
   # YOUR CODE HERE
+  max = 0
+  max2 = 0
+  for i in arr do
+    if i > max
+      max2 = max
+      max = i
+    elsif i > max2
+      max2 = i
+    end
+  end
+    return max+max2 
 end
 
 def sum_to_n? arr, n
   # YOUR CODE HERE
+  sum = false
+  for i in (0...arr.length)  do
+    for j in (i...arr.length) do
+      if arr[i]+arr[j] == n 
+        sum = true
+      end #if
+    end #for 
+  end #outer for
+  return sum
 end
 
 # Part 2
